e7d73a5f9cdb8995599efa83e18ead7db9c29a0d
JuanDDegli <juanddegliesposte@gmail.com>
2023-09-26 19:43:55 -0300

    [FEAT] config main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './index.css'
+import '../node_modules/@themesberg/flowbite/dist/flowbite.bundle.js';
+
 
 const app = createApp(App)
 
