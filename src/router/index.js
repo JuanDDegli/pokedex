9ba7c645937a68beb17f90fa6e0e693cde5f76a7
JuanDDegli <juanddegliesposte@gmail.com>
2023-09-26 19:25:03 -0300

    [FEAT] creating routes in index.js in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
+import PokemonDetailsView from '../views/PokemonDetailsView.vue';
+import FavoritesView from '../views/FavoritesView.vue';
+
+const routes = [
+  { path: '/', 
+  component: HomeView },
+
+  { path: '/pokemon/id:', 
+  component: PokemonDetailsView },
+  
+  { path: '/favorites', 
+  component: FavoritesView },
+  // Outras rotas...
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
