0f704e6b881898dc1171cefe8f7fe47dcdf8d5f7
JuanDDegli <juanddegliesposte@gmail.com>
2023-09-28 23:29:35 -0300

    [FEAT] add style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,52 @@
-<script setup>
-import { onMounted, reactive, ref, computed } from 'vue';
-import ListPokemons from '../components/ListPokemons.vue';
-import PokemonCardSelected from '../components/PokemonCardSelected.vue';
+    <script setup>
+    import { onMounted, reactive, ref } from 'vue';
+    import ListPokemons from '../components/ListPokemons.vue';
 
-let pokemons = reactive(ref([]));
+    let pokemons = reactive(ref([]));
 
-    onMounted(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=386&offset=0")
-        .then((res) => res.json())
-        .then(async (res) => {
-        const pokemonData = await Promise.all(res.results.map(async (pokemon) => {
-            const response = await fetch(pokemon.url);
-            const data = await response.json();
-            return {
-            name: data.name,
-            imageUrl: data.sprites.front_default,
-            };
-        }));
-        pokemons.value = pokemonData;
-        });
-});
-</script>
+        onMounted(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=386&offset=0")
+            .then((res) => res.json())
+            .then(async (res) => {
+            const pokemonData = await Promise.all(res.results.map(async (pokemon) => {
+                const response = await fetch(pokemon.url);
+                const data = await response.json();
+                return {
+                name: data.name,
+                imageUrl: data.sprites.front_default,
+                };
+            }));
+            pokemons.value = pokemonData;
+            });
+    });
+    </script>
 
-    <template>
+        <template>
+        <main class="bg-custom-blue-100 pl-32 pr-32 pt-32 pb-32  main-bg">
+            <div class=" grid grid-cols-3 gap-4 p-2 ">
+            <ListPokemons
+                v-for="pokemon in pokemons"
+                :key="pokemon.name"
+                :name="pokemon.name"
+                :imageUrl="pokemon.imageUrl"
+            />
+            </div>
+        </main>
+        </template>
 
-        <pokemon-card-selected />
-
-    <main class="bg-gray-300 pl-32 pr-32 pt-32 pb-32 border-solid border-2 border-red-600">
-        <div class="container rounded-xl bg-white grid grid-cols-3 gap-4 p-5 border-solid border-2 border-yellow-600 ">
-        <ListPokemons
-            v-for="pokemon in pokemons"
-            :key="pokemon.name"
-            :name="pokemon.name"
-            :imageUrl="pokemon.imageUrl"
-        />
-        </div>
-    </main>
-    </template>
+        <style scoped>
+        .main-bg {
+    background: linear-gradient(
+        to bottom right,
+        rgba(10, 10, 10, 1),
+        rgba(12, 39, 63, 1)
+        )
+        no-repeat center center fixed !important;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover !important;
+    background-position: center;
+    min-height: 100vh;
+    }
+        </style>
