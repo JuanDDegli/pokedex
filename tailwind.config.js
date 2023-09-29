548e9b77c78dbd13f22d067bbdf0138871adfc1f
JuanDDegli <juanddegliesposte@gmail.com>
2023-09-28 23:31:27 -0300

    [FEAT] add custom-colors in tailwind css and remove pokemoncard component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-blue': {
+          100:'#182737',
+        },
+        'custom-gray':{
+          500:'#1f1f1f',
+          900:'#191919'
+        }
+      },
+    },
   },
   plugins: [],
 }
