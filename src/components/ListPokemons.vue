5328ea87ff0aec7726d73c6cc93456a1998ef1bb
JuanDDegli <juanddegliesposte@gmail.com>
2023-09-28 23:28:17 -0300

    [FEAT] add style

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -1,18 +1,27 @@
-<script setup>
-defineProps({ name: String, imageUrl: String });
-</script>
+    <script setup>
+    defineProps({ name: String, imageUrl: String,  });
+    </script>
 
-<template>
-    <div class="p-2 col-span-1">
-    <div class="bg-gray-500 rounded pt-2 pb-2 m-2 mt-5 p-2 shadow-lg">
-        <p>{{ name }}</p>
-        <img :src="imageUrl" :alt="name" />
-        <button @click="openModal(pokemon)">Detalhes</button>
+    <template>
+        <div class="flex flex-col md:flex-row cursor-pointer my-auto bg-custom-blue-100 gap-2 rounded-lg p-2 shadow-xl hover:p-3 transition  delay-100  hover:-translate-y-1 hover:scale-105 duration-700 ... ">
+                <div class=" row-auto text-gray-200  hover: p-5 transition  delay-150  hover:-translate-y-1 hover:scale-105 duration-100 ... ">
+                    <img :src="imageUrl" :alt="name" class="pl-0"/>
+                    </div>
+                        <div class="my-auto mt-0 p-4 ">
+                                <div class="text-3xl overflow-ellipsis font-semibold whitespace-no-wrap overflow-hidden  text-white">
+                                    <span class=" uppercase">{{ name.charAt(0) }}</span>{{ name.slice(1) }}
+                                </div>
+                        </div>
+                </div>
+    </template>
 
-    </div>
-    </div>
-</template>
+    <style scoped>
 
-<style>
+.main-bg{
+        background-image: url('../assets/bg-gradient-blue.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
 
-</style>
+    </style>
+    
