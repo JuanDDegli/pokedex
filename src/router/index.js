8223410821d657d3def2ad8cf98bd9d46e6c0a30
JuanDDegli <juanddegliesposte@gmail.com>
2023-10-01 13:00:42 -0300

    [REFACTOR] Delete DetailsPokemon

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
-import PokemonDetailsView from '../views/PokemonDetailsView.vue';
 import FavoritesView from '../views/FavoritesView.vue';
 
 const routes = [
   { path: '/', 
   component: HomeView },
 
-  { path: '/pokemon/id:', 
-  component: PokemonDetailsView },
-  
   { path: '/favorites', 
   component: FavoritesView },
   // Outras rotas...
