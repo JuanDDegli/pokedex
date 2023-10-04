7672cb34c0ef9d610af6872f29b8e54ba53ffc3c
JuanDDegli <juanddegliesposte@gmail.com>
2023-10-04 20:52:57 -0300

    [FEAT] add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,35 @@ module.exports = {
       backgroundColor: {
         'custom-blue': {
           100:'#182737',
+          50:'#a0bceb',
+          200: '#4dd1c0',
+          
         },
         'custom-gray':{
+          50: '#b3a392',
           500:'#1f1f1f',
-          900:'#191919'
-        }
+          600: '#112643',
+          900:'#191919',
+          700: '#7d7d7d',
+          300: '#e0e0e0',
+        },
+        'custom-green':{
+          500:'#c6d320',
+        },
+        'custom-pink':{
+          50:'#fb89eb',
+          100: '#b567ce',
+        },
+        'custom-brown':{
+          100: '#7f3826',
+          200: '#c8b686',
+        },
+        'custom-purple':{
+          50:'#755573',
+        },
+        'custom-yellow':{
+          50:'#fac156',
+        },
       },
     },
   },
